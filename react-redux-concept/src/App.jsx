33290409b05605cedc23456fbc1f27b33f06465e
Maria Vanegas <marisavaneg@hotmail.com>
2026-01-08 12:06:42 -0800

    React Redux Lesson

---
+++ react-redux-concept/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+import Child1 from "./components/Child1";
+import Child2 from "./components/Child2";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <></>;
+}
+
+export default App;
